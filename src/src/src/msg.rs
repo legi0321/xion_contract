6081bc2db081b94760baa2f88e49bf83b0b3564b
legiona <74761563+legi0321@users.noreply.github.com>
2025-06-30 15:05:45 +0700

    Create msg.rs

---
+++ src/src/src/msg.rs
@@ -0,0 +1,16 @@
+use schemars::JsonSchema;
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct InstantiateMsg {}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub enum ExecuteMsg {
+    DoSomething {},
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+#[serde(rename_all = "snake_case")]
+pub enum QueryMsg {
+    GetSomething {},
+}
